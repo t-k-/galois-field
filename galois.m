a31f3a676d5c309070810b4aede37160a5227847
t-k- <clock126@126.com>
2017-10-27 18:43:25 -0400

    finished the homework

--- galois.m
+++ galois.m
@@ -7,11 +7,41 @@
 % C(x) = A(x) · B(x) = x^5 + x^3 + x^2 + x
 % C(x) = x^3 mod P(x)
 
-%   Ax = [2 1];
-%   Bx = [2 2];
-%   prim_poly = [1 0 1];
-%   Galois_visualmul(Ax, Bx, 3, prim_poly)
-%   
+%Galois_all_irreducibles(2, 4)
+%Galois_test_irreducible([1 0 0 0 1 1], 2)
+%Galois_test_irreducible([1 0 0 1 0 1], 2)
+%Galois_test_irreducible([1 0 1 0 1 1], 2)
+%Galois_all_irreducibles(3, 3)
+
+%Ax = [1 0 1];
+%Bx = [1 0 1 1];
+%Ax = [1 0 1];
+%Bx = [1 1];
+%prim_poly = [1 1 0 0 1];
+%polystring(Galois_rawmul(Ax, Bx, 2), '\n')
+%Galois_visualmul(Ax, Bx, 2, prim_poly)
+
+% Ax = [1 1 0 1 1];
+% Bx = [0 0 1 1 0 0 1 1];
+% prim_poly = [1 1 0 1 1 0 0 0 1];
+% Galois_visualmul(Ax, Bx, 2, prim_poly)
+
+%Ax = [1 1 0 0 1];
+%Bx = [1 1 0 1 1];
+%prim_poly = [1 1 0 1 1 0 0 0 1];
+%polystring(Galois_rawmul(Ax, Bx, 2), '\n')
+%Galois_visualmul(Ax, Bx, 2, prim_poly)
+
+% Ax = [1 1 0 0 1 0 1 1];
+% Bx = [1 1 0 0 0 1 0 1];
+% prim_poly = [1 1 0 1 1 0 0 0 1];
+% polystring(Galois_rawmul(Ax, Bx, 2), '\n')
+% Galois_visualmul(Ax, Bx, 2, prim_poly)
+
+prim_poly = [1 1 0 0 1];
+fprintf('~~~ generating using polynomial: '); polystring(prim_poly, ' ~~~\n')
+Galois_pow([0 1], 23, 2, prim_poly)
+
 %   Ax = [1 0 1 1];
 %   Bx = [0 1 1];
 %   prim_poly = [1 1 0 0 1];
@@ -38,26 +68,41 @@
 %   p = 2;
 %   for m = 0:4
 %   	fprintf('~~~ base = %d, degree = %d ~~~\n', p, m)
-%   	Galois_irreducible(p, m)
+%   	Galois_all_irreducibles(p, m)
 %   end
 %   
-p = 3;
-for m = 0:2
-	fprintf('~~~ base = %d, degree = %d ~~~\n', p, m)
-	Galois_irreducible(p, m)
+
+% p = 3;
+% for m = 0:2
+% 	fprintf('~~~ base = %d, degree = %d ~~~\n', p, m)
+% 	Galois_all_irreducibles(p, m)
+% end
+% 
+% prim_poly = [2 2 1];
+% fprintf('~~~ generating using polynomial: '); polystring(prim_poly, ' ~~~\n')
+% Galois_generate([1 1], 3, 2, prim_poly)
+% 
+% prim_poly = [2 1 1];
+% fprintf('~~~ generating using polynomial: '); polystring(prim_poly, ' ~~~\n')
+% Galois_generate([1 1], 3, 2, prim_poly)
+% 
+% prim_poly = [1 0 1];
+% fprintf('~~~ generating using polynomial: '); polystring(prim_poly, ' ~~~\n')
+% Galois_generate([1 1], 3, 2, prim_poly)
+
+%Galois_visualrawdiv([1 1 0 0 1], [0 0 1 1 0 0 1 1], 2)
+
+function Galois_pow(Ax, power, p, prim_poly)
+	Cx = [1];
+	for pow = 0: power
+		fprintf('(')
+		polystring(Ax, ')^')
+		fprintf('%d', pow)
+		fprintf(' = ')
+		polystring(Cx, '\n')
+		Cx = Galois_mul(Ax, Cx, p, prim_poly);
+	end
 end
-
-prim_poly = [2 2 1];
-fprintf('~~~ generating using polynomial: '); polystring(prim_poly, ' ~~~\n')
-Galois_generate([1 1], 3, 2, prim_poly)
-
-prim_poly = [2 1 1];
-fprintf('~~~ generating using polynomial: '); polystring(prim_poly, ' ~~~\n')
-Galois_generate([1 1], 3, 2, prim_poly)
-
-prim_poly = [1 0 1];
-fprintf('~~~ generating using polynomial: '); polystring(prim_poly, ' ~~~\n')
-Galois_generate([1 1], 3, 2, prim_poly)
 
 function Galois_generate(Ax, p, m, prim_poly)
 	Cx = [1];
@@ -92,12 +137,37 @@ function Galois_generate(Ax, p, m, prim_poly)
 	end
 end
 
-function Galois_irreducible(base, degree)
+function Galois_test_irreducible(Ax, base)
+	degree = find(Ax, 1, 'last') - 1;
+	[candidates, denominators] = Galois_irr_candidates(base, degree);
+	[j_rows, discard_] = size(denominators);
+
+	is_irreducible = true;
+	for j = 1:j_rows
+		j_row = denominators(j, :);
+		j_row = j_row(1:find(j_row, 1, 'last'));
+		Galois_visualrawdiv(Ax, j_row, base);
+		[Qx Rx] = Galois_rawdiv(Ax, j_row, base);
+		if length(Rx) == 0
+			is_irreducible = false;
+			break
+		end
+	end
+	if is_irreducible
+		polystring(Ax, ' ');
+		disp('is irreducible !!!');
+	end
+end
+
+function Galois_all_irreducibles(base, degree)
 	[candidates, denominators] = Galois_irr_candidates(base, degree);
 	[i_rows, discard_] = size(candidates);
 	[j_rows, discard_] = size(denominators);
 	for i = 1:i_rows
 		i_row = candidates(i, :);
+		if degree > 0 && i_row(1) == 0
+			continue
+		end
 		is_irreducible = true;
 		for j = 1:j_rows
 			j_row = denominators(j, :);
